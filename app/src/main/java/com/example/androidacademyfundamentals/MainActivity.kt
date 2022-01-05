bf7126f1392abc48ca8dac5c998bc4c982c8bbc9
Eugene92k <egorovoy92@gmail.com>
2022-01-05 21:58:43 +0700

    after third lesson

--- app/src/main/java/com/example/androidacademyfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidacademyfundamentals/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.androidacademyfundamentals
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
+        setContentView(R.layout.activity_main)
 
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, MoviesListFragment())
+            .commit()
 
     }
 }
+
+
